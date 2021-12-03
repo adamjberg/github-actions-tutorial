8001c21f8e923d2fd110b48b5db0e965be92adf7
Adam Berg <adam@xyzdigital.com>
2021-12-03 13:06:14 -0800

    Add simple express server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const App: React.FC = () => {
-  return <div>Hello Github Actions!</div>;
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("/api/message")
+      .then((res) => {
+        return res.json();
+      })
+      .then((jsonData) => {
+        setMessage(jsonData);
+      });
+  }, []);
+
+  return (
+    <>
+      <div>Hello Github Actions!</div>
+      <div>{message}</div>
+    </>
+  );
 };
---
+++ server/src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/api/message", (_, res) => {
+  return res.json({
+    data: "Hello from the server",
+  });
+});
+
+app.listen(8080);
